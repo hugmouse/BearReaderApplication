6baec3cb03190f459e9f4f4b84af3f9da9296457
hugmouse <me@mysh.dev>
2026-01-20 19:49:26 +0100

    refactor(tests): comment out unused tests, uncheck "debug executable" for testing Hopefully that will make testing faster on Github Actions

--- BearReaderApplicationTests/BearReaderApplicationTests.swift
+++ BearReaderApplicationTests/BearReaderApplicationTests.swift
@@ -5,13 +5,13 @@
 //  Created by Iaroslav Angliuster on 01.09.25.
 //
 
-import Testing
-@testable import BearReaderApplication
-
-struct BearReaderApplicationTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
+//import Testing
+//@testable import BearReaderApplication
+//
+//struct BearReaderApplicationTests {
+//
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+//
+//}
